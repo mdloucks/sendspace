ac826e1f91550c01a177c3ff03de3c6c654daa05
Matt <19471665+mdloucks@users.noreply.github.com>
2025-09-15 23:27:35 -0400

    feat: s3 mixin

---
+++ app/lib/core/mixins/s3_bucket_mixin.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+mixin S3BucketMixin {
+  /// Uploads an object to S3 and returns the public URL
+  Future<String> uploadObject({
+    required SupabaseClient client,
+    required String bucketName,
+    required File file,
+    required String objectKey,
+  }) async {
+    final response = await client.storage
+        .from(bucketName)
+        .upload(objectKey, file);
+
+    if (response.isEmpty) {
+      throw Exception('Object upload failed for {objectKey}');
+    }
+
+    return client.storage.from(bucketName).getPublicUrl(objectKey);
+  }
+}
