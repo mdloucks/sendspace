bbb1495a53fac8170596362a30587c4a51cfb008
Matt <19471665+mdloucks@users.noreply.github.com>
2025-09-16 22:46:55 -0400

    feat: add fonts

--- app/lib/core/extensions/build_context.dart
+++ app/lib/core/extensions/build_context.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sendspace/theme/text_theme.dart';
 
 extension ThemeModeExtension on BuildContext {
   bool get isDarkMode =>
       MediaQuery.of(this).platformBrightness == Brightness.dark;
-  ColorScheme get colorTheme => Theme.of(this).colorScheme;
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
   TextTheme get textTheme => Theme.of(this).textTheme;
+}
+
+extension FontModifier on TextTheme {
+  TextTheme permanentMarker() {
+    return applyFontToTextTheme(this, GoogleFonts.permanentMarker);
+  }
 }
